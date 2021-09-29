0a8b67e1c106bcc6a13a2362c1053b4030e18e85
Lyudmila Bakaeva <bakaeva@corepartners.ru>
2021-09-29 15:10:57 +0300

    add accordion.js - реализация аккордеона

---
+++ js/accordion.js
@@ -0,0 +1,20 @@
+const featureLinks = document.querySelectorAll(".feature__link");
+const featureSubs = document.querySelectorAll(".feature-sub");
+let selectedItem = 0;
+
+featureLinks.forEach((featureLink, ind) => {
+	featureLink.addEventListener('click', () => {
+		console.log('selectedItem = ', selectedItem, 'ind = ', ind);
+		if (ind !== selectedItem)
+		{
+			featureLinks[selectedItem].classList.remove('feature__link_active');
+			featureSubs[selectedItem].classList.add('hidden');
+			featureLink.classList.add('feature__link_active');
+			featureSubs[ind].classList.remove('hidden');
+			selectedItem = ind;
+		}	else {
+			featureLink.classList.toggle('feature__link_active');
+			featureSubs[ind].classList.toggle('hidden');
+		}
+	});
+});
