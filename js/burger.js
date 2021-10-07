48ae27f9ca85d78ed440c622cdda2600f535ba02
Lyudmila Bakaeva <l_bakaeva74@mail.ru>
2021-10-07 16:29:46 +0300

    fix burger menu

--- js/burger.js
+++ js/burger.js
@@ -2,12 +2,13 @@ const burgerMenu = document.querySelector(".menu");
 const burgerMenuBtn = document.querySelector(".humburger-menu");
 const menuLinks = document.querySelectorAll('.menu-list__link');
 
-const toggleMenu = () => {
+burgerMenuBtn.addEventListener('click', () => {
 	burgerMenu.classList.toggle('menu-active');
 	burgerMenuBtn.classList.toggle('humburger-menu-active');
-};
-
-burgerMenuBtn.addEventListener('click', () => toggleMenu());
+});
 for (let link of menuLinks) {
-	link.addEventListener('click', () => toggleMenu());
+	link.addEventListener('click', () => {
+		burgerMenu.classList.remove('menu-active');
+		burgerMenuBtn.classList.remove('humburger-menu-active');
+	});
 }
